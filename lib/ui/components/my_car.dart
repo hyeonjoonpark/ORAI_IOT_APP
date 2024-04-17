f794467b308e9ea95e9fd3952a618466c634c63a
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-17 14:34:37 +0900

    FEAT: car info

--- lib/ui/components/my_car.dart
+++ lib/ui/components/my_car.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:orai/ui/components/nav_bar.dart';
@@ -19,7 +20,7 @@ class _MyCarWidgetState extends State<MyCarWidget> {
           child: Column(
             children: [
               Container(
-                margin: EdgeInsets.only(left: 0.05.sw, top: 0.03.sh),
+                margin: EdgeInsets.only(left: 0.07.sw, top: 0.03.sh),
                 alignment: Alignment.centerLeft,
                 child: const Text(
                   "내 차량 정보",
@@ -27,6 +28,35 @@ class _MyCarWidgetState extends State<MyCarWidget> {
                     fontSize: 16,
                     fontWeight: FontWeight.w900,
                   ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 0.9.sw,
+                height: 0.1.sh,
+                alignment: Alignment.centerLeft,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 176, 176, 176),
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                ),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "차량 번호 : ",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    Text(
+                      "00가 0000",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
