f127871e46fb0f83c6059d695de0a6dcef6e8978
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-22 13:31:50 +0900

    commit

--- lib/ui/components/map.dart
+++ lib/ui/components/map.dart
@@ -4,33 +4,31 @@ import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 Widget buildMap(LatLng currentLocation) {
-  return Expanded(
-    child: FlutterMap(
-      options: MapOptions(
-        // currentLocation이 null이 아니면 currentLocation을 사용하고, null이면 서울의 좌표를 기본값으로 사용합니다.
-        initialCenter: currentLocation,
-        initialZoom: 13.0,
-        maxZoom: 18.0,
-        minZoom: 10.0,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        ),
-        RichAttributionWidget(
-          attributions: [
-            TextSourceAttribution(
-              'OpenStreetMap 기여자들',
-              onTap: () async {
-                final Uri url = Uri.parse('https://www.openstreetmap.org');
-                if (await canLaunchUrl(url)) {
-                  await launchUrl(url);
-                }
-              },
-            ),
-          ],
-        ),
-      ],
+  return FlutterMap(
+    options: MapOptions(
+      // currentLocation이 null이 아니면 currentLocation을 사용하고, null이면 서울의 좌표를 기본값으로 사용합니다.
+      initialCenter: currentLocation,
+      initialZoom: 13.0,
+      maxZoom: 18.0,
+      minZoom: 10.0,
     ),
+    children: [
+      TileLayer(
+        urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      ),
+      RichAttributionWidget(
+        attributions: [
+          TextSourceAttribution(
+            'OpenStreetMap 기여자들',
+            onTap: () async {
+              final Uri url = Uri.parse('https://www.openstreetmap.org');
+              if (await canLaunchUrl(url)) {
+                await launchUrl(url);
+              }
+            },
+          ),
+        ],
+      ),
+    ],
   );
 }
