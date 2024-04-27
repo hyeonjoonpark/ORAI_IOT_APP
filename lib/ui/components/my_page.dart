03daa4533b2a3cec056797e00f732d85568751ef
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-27 12:54:49 +0900

    CHANGE : stateful -> stateless

--- lib/ui/components/my_page.dart
+++ lib/ui/components/my_page.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/cupertino.dart';
 
-class MyPageWidget extends StatefulWidget {
+class MyPageWidget extends StatelessWidget {
   const MyPageWidget({super.key});
 
-  @override
-  State<MyPageWidget> createState() => _MyPageWidgetState();
-}
-
-class _MyPageWidgetState extends State<MyPageWidget> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
