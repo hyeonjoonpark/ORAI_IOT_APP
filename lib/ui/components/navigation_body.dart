017abffe19cb3def962763fe067bafb08d4ccd38
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-27 12:40:57 +0900

    REMOVE : import

--- lib/ui/components/navigation_body.dart
+++ lib/ui/components/navigation_body.dart
@@ -3,7 +3,6 @@ import 'package:orai/provider/bottom_navigation_provider.dart';
 import 'package:orai/ui/components/home_widget.dart';
 import 'package:orai/ui/components/my_car.dart';
 import 'package:orai/ui/components/my_page.dart';
-import 'package:orai/ui/components/parking_lot_widget.dart';
 
 Widget navigationBody(BottomNavigationProvider provider) {
   return provider.currentPage == 0
