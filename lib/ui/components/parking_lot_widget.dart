1b64d7992ef0a01f7eb1b732984aa63ab7db7a2f
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-27 13:00:55 +0900

    REMOVE : navBar

--- lib/ui/components/parking_lot_widget.dart
+++ lib/ui/components/parking_lot_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:orai/ui/components/nav_bar.dart';
 
 class ParkingLotWidget extends StatefulWidget {
   const ParkingLotWidget({super.key});
@@ -22,7 +21,6 @@ class _ParkingLotWidgetState extends State<ParkingLotWidget> {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       builder: (context, child) => Scaffold(
-        appBar: navBar(),
         body: SingleChildScrollView(
           child: Center(
             child: Column(
